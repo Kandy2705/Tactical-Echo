6d13f3a93cdc897dc3e65ef90967aa8284d2d959
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-09-09 23:54:26 +0700

    feat: expand player input reader for movement and aiming

--- Assets/_Project/Character/Player/PlayerInputReader.cs
+++ Assets/_Project/Character/Player/PlayerInputReader.cs
@@ -6,23 +6,32 @@ namespace TacticalEcho.Character.Player
 {
     public sealed class PlayerInputReader : MonoBehaviour
     {
+        [Header("Movement")]
         [SerializeField] private InputActionReference moveAction;
         [SerializeField] private InputActionReference lookAction;
+        [SerializeField] private InputActionReference sprintAction;
+
+        [Header("Combat")]
         [SerializeField] private InputActionReference fireAction;
         [SerializeField] private InputActionReference reloadAction;
+        [SerializeField] private InputActionReference aimAction;
 
         public event Action FireRequested;
         public event Action ReloadRequested;
 
-        public Vector2 Move => moveAction != null ? moveAction.action.ReadValue<Vector2>() : Vector2.zero;
-        public Vector2 Look => lookAction != null ? lookAction.action.ReadValue<Vector2>() : Vector2.zero;
+        public Vector2 Move => ReadVector2(moveAction);
+        public Vector2 Look => ReadVector2(lookAction);
+        public bool IsSprinting => IsPressed(sprintAction);
+        public bool IsAiming => IsPressed(aimAction);
 
         private void OnEnable()
         {
-            Enable(moveAction);
-            Enable(lookAction);
-            Enable(fireAction);
-            Enable(reloadAction);
+            SetEnabled(moveAction, true);
+            SetEnabled(lookAction, true);
+            SetEnabled(sprintAction, true);
+            SetEnabled(fireAction, true);
+            SetEnabled(reloadAction, true);
+            SetEnabled(aimAction, true);
 
             if (fireAction != null)
             {
@@ -47,16 +56,51 @@ namespace TacticalEcho.Character.Player
                 reloadAction.action.performed -= OnReloadPerformed;
             }
 
-            Disable(moveAction);
-            Disable(lookAction);
-            Disable(fireAction);
-            Disable(reloadAction);
+            SetEnabled(moveAction, false);
+            SetEnabled(lookAction, false);
+            SetEnabled(sprintAction, false);
+            SetEnabled(fireAction, false);
+            SetEnabled(reloadAction, false);
+            SetEnabled(aimAction, false);
         }
 
-        private void OnFirePerformed(InputAction.CallbackContext context) => FireRequested?.Invoke();
-        private void OnReloadPerformed(InputAction.CallbackContext context) => ReloadRequested?.Invoke();
+        private void OnFirePerformed(InputAction.CallbackContext context)
+        {
+            FireRequested?.Invoke();
+        }
 
-        private static void Enable(InputActionReference actionReference) => actionReference?.action.Enable();
-        private static void Disable(InputActionReference actionReference) => actionReference?.action.Disable();
+        private void OnReloadPerformed(InputAction.CallbackContext context)
+        {
+            ReloadRequested?.Invoke();
+        }
+
+        private static Vector2 ReadVector2(InputActionReference actionReference)
+        {
+            return actionReference != null
+                ? actionReference.action.ReadValue<Vector2>()
+                : Vector2.zero;
+        }
+
+        private static bool IsPressed(InputActionReference actionReference)
+        {
+            return actionReference != null && actionReference.action.IsPressed();
+        }
+
+        private static void SetEnabled(InputActionReference actionReference, bool enabled)
+        {
+            if (actionReference == null)
+            {
+                return;
+            }
+
+            if (enabled)
+            {
+                actionReference.action.Enable();
+            }
+            else
+            {
+                actionReference.action.Disable();
+            }
+        }
     }
 }
